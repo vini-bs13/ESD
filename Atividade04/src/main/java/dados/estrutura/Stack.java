604a40dd5529c80cb5719efaf0aa9ddf62deb01c
vini-bs13 <viniprogramador13@gmail.com>
2026-09-21 14:45:07 -0300

    Começando ativididade do labirinto com pilha

---
+++ Atividade04/src/main/java/dados/estrutura/Stack.java
@@ -0,0 +1,37 @@
+package dados.estrutura;
+
+public class Stack<T extends Comparable<T>> {
+
+    private Vetor<T> vetor;
+
+
+    public Stack(int capacidade){
+        vetor = new Vetor<>(capacidade);
+    }
+
+    public void push(T elemento){
+        vetor.inserir(elemento);
+    }
+
+    public boolean isEmpty(){
+        return vetor.obterTamanho() == 0;
+    }
+
+    public T pop(){
+        if(isEmpty()) {
+            throw new RuntimeException("Vazio");
+        }
+
+        T valor = vetor.ler(vetor.obterTamanho()-1);
+        vetor.remover(vetor.obterTamanho()-1);
+        return valor;
+    }
+
+    public T peek(){
+        if (isEmpty()){
+            throw new RuntimeException("Vazio");
+        }
+        return vetor.ler(vetor.obterTamanho()-1);
+    }
+
+}
